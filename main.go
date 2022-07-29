1833190406791fc1abd1cacc56e404eed87cc7f4
shoyalax0703 <shoya.lax@gmail.com>
2022-07-29 14:43:13 +0900

    completed to build simple webserver creation with go

--- main.go
+++ main.go
@@ -1,15 +1,46 @@
 package main
 
-import(
+import (
 	"fmt"
-  "log"
+	"log"
 	"net/http"
-
 )
 
-func(){
-	fileServer := http.FileServer( http.Dir("./static")
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseFrom() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 
+}
 
-	)
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method NOt Allowed", http.StatusNotFound)
+		return
+
+	}
+	fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+
+	}
+
 }
